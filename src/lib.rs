1a495baa2dd08c8c9d04080a5ffc31e80c305dc6
Uzair Aftab <uzaaft@outlook.com>
2023-01-21 20:33:01 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+struct Length<B, V >{
+    BaseUnit: T,
+    Value: 
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
